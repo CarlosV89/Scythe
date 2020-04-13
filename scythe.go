ad463748d01f08f4ea160a92c95eef51d2a22732
Carlos Eduardo Villalta Figueroa <cevillalta07@gmail.com>
2020-04-12 20:01:17 -0600

    add readme fix some issues present with root cmd and where commands were getting added

--- scythe.go
+++ scythe.go
@@ -15,15 +15,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/vanor89/scythe/cmd"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		fmt.Printf("%+v", err)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
