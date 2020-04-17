74ef7c2f37968d7abc12f6eb222ff17c02fafd47
Carlos Eduardo Villalta Figueroa <cevillalta07@gmail.com>
2020-04-16 22:58:31 -0600

    swap pc

---
+++ cmd/init_test.go
@@ -0,0 +1,16 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/vanor89/scythe/internal/platform/testing/mock"
+)
+
+func Test_Init(t *testing.T) {
+	mockCmd := mock.CobraCommand()
+
+	args := []string{"--arg", "test-arg"}
+	if err := sow(mockCmd, args); err != nil {
+		t.Fatal(err)
+	}
+}
